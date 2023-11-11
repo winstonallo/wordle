b63a3a7b52120a953037ac09672df45b41aad0dd
Josef Koupy <jkoupy@c2r3p1.42vienna.com>
2023-11-11 11:36:09 +0100

    checker.c check_word

--- src/checker.c
+++ src/checker.c
@@ -6,7 +6,72 @@
 /*   By: jkoupy <jkoupy@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/11 11:06:57 by jkoupy            #+#    #+#             */
-/*   Updated: 2023/11/11 11:06:58 by jkoupy           ###   ########.fr       */
+/*   Updated: 2023/11/11 11:35:34 by jkoupy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+//structure
+// char *word  "*****"
+// char *guess  "*****"
+// char *color  "wwwww" each for color . w-grey, g-green, y-yellow
+
+#include <string.h>
+
+typedef struct s_checker
+{
+	char	*word;
+	char	*guess;
+	char	*color;
+}	t_checker;
+
+int	check_correct(t_checker *a)
+{
+	int	i;
+
+	i = 0;
+	while (i < 5)
+	{
+		if (a->word[i] == a->guess[i])
+		{
+			a->color[i] = 'g';
+		}
+		i++;
+	}
+}
+
+void	check_misplaced(t_checker *a)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (i < 5)
+	{
+		if (a->color == 'g' || a->color == 'y')
+			continue ;
+		j = 0;
+		while (j < 5)
+		{
+			if (a->word[i] == a->guess[j])
+			{
+				a->color[i] = 'y';
+				break ;
+			}
+			j++;
+		}
+		i++;
+	}
+}
+
+t_checker	check_word(char *word, char *guess)
+{
+	t_checker	a;
+
+	strcpy(a.color, "wwwww");
+	strcpy(a.word, word);
+	strcpy(a.guess, guess);
+	if (check_correct(&a))
+		return (a);
+	check_misplaced(&a);
+	return (a);
+}
